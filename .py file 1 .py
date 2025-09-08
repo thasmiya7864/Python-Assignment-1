93d0d333bbfa97b7bfeb39533096f4d3fdeaa47b
Dudekula Thasmiya <thasmiya7864@gmail.com>
2025-09-08 20:32:16 +0530

    initial commit

---
+++ .py file 1 .py
@@ -0,0 +1,14 @@
+# Basic Mathematical Operations
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+Addition = num1+num2
+Subtraction = num1-num2
+Multiplication = num1*num2
+Division = num1/num2
+
+print("Addition:",Addition)
+print("Subtraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Division:",Division)
